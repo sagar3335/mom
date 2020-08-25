b2e2be42f8b08af66cb9335ebb03e515f597494d
sagar <suvankar.qa@gmail.com>
2020-08-25 11:28:12 +0530

    added print

--- src/StringProgram/PremutationString.java
+++ src/StringProgram/PremutationString.java
@@ -4,8 +4,8 @@ public class PremutationString {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("sgar");
-System.out.println("senapati");
+System.out.println("channge");
+System.out.println("added");
 	}
 
 }
