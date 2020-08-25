92e08c486acdbcb9fcaaeeb970cd90ba0dddbe52
sagar <suvankar.qa@gmail.com>
2020-08-25 11:31:41 +0530

    change file

--- src/StringProgram/PremutationString.java
+++ src/StringProgram/PremutationString.java
@@ -5,7 +5,7 @@ public class PremutationString {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("channge");
-System.out.println("added");
+System.out.println("sysy");
 	}
 
 }
